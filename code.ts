5ce50b222807b6e73cca249ba626d2ef79aeddd3
Maxime <maxime.daraize@gmail.com>
2022-07-28 10:04:32 -0400

    feat: color styles selection and apply to project

--- code.ts
+++ code.ts
@@ -1,3 +1,18 @@
+let colorStylesId = [];
+let colorStylesValues = []
+let countStyles = [];
+figma.getLocalPaintStyles().forEach(sytleId => colorStylesId.push(sytleId.id))
+
+colorStylesId.forEach(id => {
+  return colorStylesValues.push(figma.getStyleById(id).paints[0].color);
+  // return colorStylesValues.push(figma.getStyleById(id).paints[0].color)
+})
+
+console.log('#0', figma.getLocalPaintStyles());
+
+console.log('#1', colorStylesId);
+console.log(colorStylesValues)
+
 // This plugin will open a window to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
@@ -14,19 +29,42 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+  if (msg.type === 'apply-styles') {
+    if (msg.styleCategory.color === true) {
+      function traverse(node) {
+        if ("children" in node) {
+          if (node.type != "INSTANCE") {
+            for (const child of node.children) {
+              traverse(child)
+              if (!child.name.match(/Page/)) { //return all node expect Page
+                console.log('child', child)
+                console.log('#3', child.fills[0].color);
+                console.log('#4', colorStylesValues[0]);
+                console.log('#5', JSON.stringify(colorStylesValues).includes(JSON.stringify(child.fills[0].color)));
+                console.log('$$$', JSON.stringify(child.fills[0].color))
+                if (child.fillStyleId === '' && JSON.stringify(colorStylesValues).includes(JSON.stringify(child.fills[0].color))) {           
+                  let styleId = '';
+                  figma.getLocalPaintStyles().forEach(item => {
+                    console.log('$$$', item)
+                    if (JSON.stringify(item.paints[0].color) === JSON.stringify(child.fills[0].color)) {
+                      console.log('@@@', item.id)
 
+                      countStyles.push(item.id)
+                       styleId = item.id
+                      return styleId
+                    }
+                  })
+                  child.fillStyleId = styleId;
+                }
+              }
+            }
+          }
+        }
+      }
+      traverse(figma.root) // start the traversal at the root
+      figma.notify(countStyles.length > 0 ? `${countStyles.length} styles applied` : 'No styles applied');
+    }
+  }
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
